ba75eea79b910e455fe0517ca7eb0310a053c3d1
cepthomas <cepthomas@gmail.com>
2026-02-16 11:50:48 -0600

    Polish tests.

--- Test/Program.cs
+++ Test/Program.cs
@@ -16,8 +16,8 @@ namespace Ephemera.NebOsc.Test
         static void Main()
         {
             TestRunner runner = new(OutputFormat.Readable);
-            string[] cases = new string[] { "OSC" };
-            runner.RunSuites(cases);
+            string[] torun = new string[] { "OSC" };
+            runner.RunSuites(torun);
             var fn = Path.Combine(MiscUtils.GetSourcePath(), "test_out.txt");
             File.WriteAllLines(fn, runner.Context.OutputLines);
         }
@@ -37,30 +37,23 @@ namespace Ephemera.NebOsc.Test
                 TimeTag tt2copy = new(new TimeTag(dt2)); // copy constructor
 
                 // Check them all.
-                UT_EQUAL(ttImmediate.ToString(), "When:Immediate");
-                UT_EQUAL(tt1.ToString(), "When:2005-05-09 15:47:39.000 Seconds:3324642459 Fraction:528280977");
-                UT_EQUAL(tt2.ToString(), "When:2022-11-24 07:29:06.000 Seconds:3878263746 Fraction:3440268803");
+                Assert(ttImmediate.ToString() == "When:Immediate");
+                Assert(tt1.ToString() == "When:2005-05-09 15:47:39.000 Seconds:3324642459 Fraction:528280977");
+                Assert(tt2.ToString() == "When:2022-11-24 07:29:06.000 Seconds:3878263746 Fraction:3440268803");
 
-                UT_TRUE(tt1.Equals(tt1));
-                UT_FALSE(ttImmediate.Equals(tt2));
-                UT_FALSE(tt1raw.Equals(tt1));
+                Assert(tt1.Equals(tt1));
+                Assert(!ttImmediate.Equals(tt2));
+                Assert(!tt1raw.Equals(tt1));
 
-                UT_TRUE(tt1 == tt1raw);
-                UT_TRUE(tt2 == tt2copy);
-                UT_TRUE(tt1 != tt2);
-                UT_FALSE(tt1 != tt1raw);
-                UT_FALSE(tt2 != tt2copy);
-                UT_FALSE(tt1 == tt2);
+                Assert(tt1 == tt1raw);
+                Assert(tt2 == tt2copy);
+                Assert(tt1 != tt2);
 
-                UT_TRUE(tt2 >= tt1);
-                UT_TRUE(tt2 > tt1);
-                UT_FALSE(tt1 >= tt2);
-                UT_FALSE(tt1 > tt2);
+                Assert(tt2 >= tt1);
+                Assert(tt2 > tt1);
 
-                UT_TRUE(tt1 <= tt2);
-                UT_TRUE(tt1 < tt2);
-                UT_FALSE(tt2 <= tt1);
-                UT_FALSE(tt2 < tt1);
+                Assert(tt1 <= tt2);
+                Assert(tt1 < tt2);
             }
         }
 
@@ -74,55 +67,55 @@ namespace Ephemera.NebOsc.Test
 
                 // pack
                 bytes = Packet.Pack("Abe*-88= XXXq");
-                UT_EQUAL(bytes.Count, 16);
+                Assert(bytes.Count == 16);
                 // unpack
                 string sval = "";
                 start = 0;
                 ok = Packet.Unpack(bytes.ToArray(), ref start, ref sval);
-                UT_TRUE(ok);
-                UT_EQUAL(sval, "Abe*-88= XXXq");
+                Assert(ok);
+                Assert(sval == "Abe*-88= XXXq");
 
                 // pack
                 bytes = Packet.Pack(193082);
-                UT_EQUAL(bytes.Count, 4);
+                Assert(bytes.Count == 4);
                 // unpack
                 int ival = 0;
                 start = 0;
                 ok = Packet.Unpack(bytes.ToArray(), ref start, ref ival);
-                UT_TRUE(ok);
-                UT_EQUAL(ival, 193082);
+                Assert(ok);
+                Assert(ival == 193082);
 
                 // pack
                 bytes = Packet.Pack(7340912L);
-                UT_EQUAL(bytes.Count, 8);
+                Assert(bytes.Count == 8);
                 // unpack
                 ulong uval = 0;
                 start = 0;
                 ok = Packet.Unpack(bytes.ToArray(), ref start, ref uval);
-                UT_TRUE(ok);
-                UT_EQUAL(uval, (ulong)7340912);
+                Assert(ok);
+                Assert(uval == (ulong)7340912);
 
                 // pack
                 bytes = Packet.Pack(2965.8345f);
-                UT_EQUAL(bytes.Count, 4);
+                Assert(bytes.Count == 4);
                 // unpack
                 float fval = 0;
                 start = 0;
                 ok = Packet.Unpack(bytes.ToArray(), ref start, ref fval);
-                UT_TRUE(ok);
-                UT_EQUAL(fval, 2965.8345f);
+                Assert(ok);
+                Assert(fval == 2965.8345f);
 
                 // pack
                 bytes = Packet.Pack(new List<byte>() { 11, 28, 205, 68, 137, 251, 59, 71, 184 });
-                UT_EQUAL(bytes.Count, 16);
+                Assert(bytes.Count == 16);
                 // unpack
                 List<byte> bval = new();
                 start = 0;
                 ok = Packet.Unpack(bytes.ToArray(), ref start, ref bval);
-                UT_TRUE(ok);
-                UT_EQUAL(bval.Count, 9);
-                UT_EQUAL(bval[3], 68);
-                UT_EQUAL(bval[8], 184);
+                Assert(ok);
+                Assert(bval.Count == 9);
+                Assert(bval[3] == 68);
+                Assert(bval[8] == 184);
             }
         }
 
@@ -139,26 +132,26 @@ namespace Ephemera.NebOsc.Test
 
                 var packed = m1.Pack();
 
-                UT_NOT_EQUAL(packed.Count, 0);
-                UT_EQUAL(packed.Count, 52);
+                Assert(packed.Count != 0);
+                Assert(packed.Count == 52);
 
                 Message m2 = new();
                 bool valid = m2.Unpack(packed.ToArray());
 
-                UT_TRUE(valid);
-                UT_EQUAL(m2.Address, m1.Address);
-                UT_EQUAL(m2.Data.Count, m1.Data.Count);
-                UT_EQUAL(m2.Data[0].ToString()!, m1.Data[0].ToString()!);
-                UT_EQUAL(m2.Data[1].ToString()!, m1.Data[1].ToString()!);
-                UT_EQUAL(m2.Data[2].ToString()!, m1.Data[2].ToString()!);
-                UT_EQUAL(m2.Data[3].ToString()!, m1.Data[3].ToString()!);
+                Assert(valid);
+                Assert(m2.Address == m1.Address);
+                Assert(m2.Data.Count == m1.Data.Count);
+                Assert(m2.Data[0].ToString()! == m1.Data[0].ToString()!);
+                Assert(m2.Data[1].ToString()! == m1.Data[1].ToString()!);
+                Assert(m2.Data[2].ToString()! == m1.Data[2].ToString()!);
+                Assert(m2.Data[3].ToString()! == m1.Data[3].ToString()!);
 
                 // Add some invalid data.
                 m1.Data.Add(new List<double>());
                 packed = m1.Pack();
-                UT_EQUAL(packed.Count, 0);
-                UT_EQUAL(m1.Errors.Count, 1);
-                UT_EQUAL(m1.Errors[0], "Unknown type: System.Collections.Generic.List`1[System.Double]");
+                Assert(packed.Count == 0);
+                Assert(m1.Errors.Count == 1);
+                Assert(m1.Errors[0] == "Unknown type: System.Collections.Generic.List`1[System.Double]");
             }
         }
 
@@ -194,15 +187,15 @@ namespace Ephemera.NebOsc.Test
                 m1.Data.Add(199.44);
                 m1.Data.Add("Snafu boss-man");
                 bool ok = nout.Send(m1);
-                UT_TRUE(ok);
+                Assert(ok);
 
                 // Wait a bit.
                 System.Threading.Thread.Sleep(500);
 
                 // What happened.
-                UT_EQUAL(logs.Count, 0);
-                logs.ForEach(l => UT_INFO(l));
-                UT_EQUAL(rxMsgs.Count, 1);
+                Assert(logs.Count == 0);
+                logs.ForEach(l => Info(l));
+                Assert(rxMsgs.Count == 1);
             }
         }
     }
